a9a9c1c313ebba20905cf7affa63d2f9c6827540
David Charte <fdavidcl@protonmail.com>
2018-01-13 19:30:06 +0100

    Remove old files

--- R/util.R
+++
@@ -1,5 +0,0 @@
-tryRequire <- function(package) {
-  if (!requireNamespace(package, quietly = TRUE)) {
-    stop(paste0("rutavis: Package '", package, "' is not installed and is needed for autoencoder functionality"))
-  }
-}
