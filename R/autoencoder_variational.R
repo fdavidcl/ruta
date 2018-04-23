284d45cdb9c5d4308744030f531ed2b3e38ad4e7
David Charte <fdavidcl@protonmail.com>
2018-04-23 12:52:41 +0200

    Parametrize network in VAE

--- R/autoencoder_variational.R
+++ R/autoencoder_variational.R
@@ -9,18 +9,35 @@
 #' @param latent_dim Latent space dimension
 #' @param loss Reconstruction error to be combined with KL divergence in order to compute
 #'   the variational loss
+#' @import purrr
 #' @export
-autoencoder_variational <- function(intermediate, latent_dim, loss = "binary_crossentropy") {
-  structure(
-    list(
-      intermediate = intermediate,
-      latent_dim = latent_dim,
-      loss = variational_loss(loss)
-    ),
-    class = c(ruta_autoencoder_variational, ruta_autoencoder)
-  )
+autoencoder_variational <- function(network, loss = "binary_crossentropy", auto_transform_network = TRUE) {
+  network <- as_network(network)
+
+  if (detect_index(network, ~ ruta_layer_variational %in% class(.)) == 0) {
+    if (auto_transform_network) {
+      message("Transforming encoding layer into variational block")
+      encoding_units <- network[[network %@% "encoding"]]$units
+      network[network %@% "encoding"] <- variational_block(encoding_units)
+    } else {
+      stop("Can't build a variational autoencoder without a variational block")
+    }
+  }
+
+  new_autoencoder(network, variational_loss(loss), extra_class = ruta_autoencoder_variational)
 }
 
+#' Create a variational block of layers
+#'
+#' This variational block consists in two dense layers which take as input the previous layer
+#' and a sampling layer. More specifically, these layers aim to represent the mean and the
+#' log variance of the learned distribution in a variational autoencoder.
+#' @param units Number of units
+#' @return A construct with class \code{"ruta_layer"}
+#' @examples
+#' variational_block(3)
+#' @family neural layers
+#' @export
 variational_block <- function(units) {
   make_atomic_network(ruta_layer_variational, units, "linear")
 }
@@ -45,47 +62,34 @@ to_keras.ruta_layer_variational <- function(x, input_shape, model = keras::keras
   }
 
   # "output_shape" isn't necessary with the TensorFlow backend
-  z <- keras::layer_concatenate(list(z_mean, z_log_var)) %>%
-    keras::layer_lambda(sampling)
-
-  list(
-    latent = z_mean,
-    sampled = z
-  )
+  keras::layer_concatenate(list(z_mean, z_log_var)) %>%
+    keras::layer_lambda(sampling, name = "sampling")
 }
 
+#' @rdname to_keras.ruta_autoencoder
+#' @import purrr
+#' @export
 to_keras.ruta_autoencoder_variational <- function(learner, input_shape) {
-  original_dim <- input_shape
-  intermediate_dim <- learner$intermediate
-  latent_dim <- learner$latent_dim
-
-  x <- keras::layer_input(shape = c(original_dim))
-  h <- x
-  #for (intermediate_dim in intermediate) {
-    h <- keras::layer_dense(h, intermediate_dim, activation = "selu")
-  #}
-
-  encodings <- to_keras(variational_block(latent_dim)[[1]], input_shape, model = h)
-  z_mean <- encodings$latent
-  z <- encodings$sampled
-
-  # we instantiate these layers separately so as to reuse them later
-  decoder_h <- keras::layer_dense(units = intermediate_dim, activation = "selu")
-  decoder_mean <- keras::layer_dense(units = original_dim, activation = "sigmoid")
-  h_decoded <- decoder_h(z)
-  x_decoded_mean <- decoder_mean(h_decoded)
-
   # end-to-end autoencoder
-  vae <- keras::keras_model(x, x_decoded_mean)
+  vae <- to_keras(learner$network, input_shape)
 
   # encoder, from inputs to latent space
-  encoder <- keras::keras_model(x, z_mean)
+  encoder <- keras::keras_model(vae$input, keras::get_layer(vae, "z_mean")$output)
 
   # generator, from latent space to reconstructed inputs
-  decoder_input <- keras::layer_input(shape = latent_dim)
-  h_decoded_2 <- decoder_h(decoder_input)
-  x_decoded_mean_2 <- decoder_mean(h_decoded_2)
-  generator <- keras::keras_model(decoder_input, x_decoded_mean_2)
+  decoder_input <- keras::layer_input(shape = keras::get_layer(vae, "sampling")$output_shape[[2]])
+
+  # re-build the decoder taking layers from the model
+  start <- detect_index(vae$layers, ~ .$name == "sampling")
+  end <- length(vae$layers) - 1
+
+  gen_layer <- decoder_input
+  for (l in start:end) {
+    # zero-based index
+    gen_layer <- keras::get_layer(vae, index = l)(gen_layer)
+  }
+
+  generator <- keras::keras_model(decoder_input, gen_layer)
 
   list(
     autoencoder = vae,
@@ -119,7 +123,6 @@ to_keras.ruta_loss_variational <- function(loss, model) {
 #' @export
 generate.ruta_autoencoder_variational <- function(learner, dimensions = c(1, 2), from = 0.05, to = 0.95, side = 10, fixed_values = 0.5) {
   d <- learner$models$decoder$input_shape[[2]]
-  cat(d)
   md <- length(dimensions)
   col <- seq(from = from, to = to, length.out = side) %>% qnorm()
 
@@ -127,10 +130,17 @@ generate.ruta_autoencoder_variational <- function(learner, dimensions = c(1, 2),
   names(args) <- paste("D", dimensions)
   moving_dims <- cross_df(args)
 
-  fixed <- rep(fixed_values, times = side ** md) %>% qnorm()
+  # TODO Allow for different fixed values in each constant dimension
+  encoded <-
+    fixed_values %>%
+    rep(side ** md) %>%
+    qnorm() %>%
+    list() %>%
+    rep(d) %>%
+    data.frame()
 
-  encoded <- data.frame(rep(list(fixed), d))
   encoded[, dimensions] <- moving_dims
-  # encoded <- data.frame(col1, col2) %>% as.matrix()
-  sampled <- model %>% decode(encoded)
+  encoded <- as.matrix(encoded)
+
+  sampled <- decode(model, encoded)
 }
