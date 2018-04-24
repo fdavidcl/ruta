214df55a7abb3e72b1621ccd57a7369aa2c393f8
David Charte <fdavidcl@protonmail.com>
2018-04-25 00:56:33 +0200

    Common name for generic parameters
    Refs #8

--- R/generics.R
+++ R/generics.R
@@ -9,11 +9,18 @@ as_loss <- function(x) UseMethod("as_loss")
 #' Coercion to ruta_network
 #'
 #' Generic function to coerce objects into networks.
-#' @param object Object to be converted into a network
+#' @param x Object to be converted into a network
 #' @return A \code{"ruta_network"} construct
 #' @export
-as_network <- function(object) UseMethod("as_network")
+as_network <- function(x) UseMethod("as_network")
 
+#' Convert a Ruta object onto Keras objects and functions
+#'
+#' Generic function which uses the Keras API to build objects
+#' out of Ruts wrappers
+#'
+#' @param x Object to be converted
+#' @param ...
 to_keras <- function(x, ...) UseMethod("to_keras")
 
 #' Generate samples from a generative model
