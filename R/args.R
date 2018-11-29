abad8e02a47c9c802081a6eefc9811956c213d50
David Charte <fdavidcl@protonmail.com>
2018-11-29 14:08:02 +0100

    Printable arg checks

--- R/args.R
+++ R/args.R
@@ -36,8 +36,14 @@ arg_network <- arg_constructor("ruta_network", .required = TRUE)
 # Argument descriptor for a loss function
 arg_loss <- arg_constructor(
   c("ruta_loss", "character"),
-  c("mean_squared_error"),
+  list_keras_objects("loss"),
   "mean_squared_error"
+)
+
+arg_activation <- arg_constructor(
+  c("character"),
+  list_keras_objects("activation"),
+  "linear"
 )
 
 which_functions <- function() as.character(lsf.str("package:ruta"))
@@ -58,7 +64,29 @@ which_args <- function(f) {
 
   names(checks) <- names(defaults)
   # Return descriptions for each argument
-  checks
+  structure(checks, class = "ruta_args")
+}
+
+print.ruta_args <- function(checks) {
+  cat("Usage: \n")
+  for (arg in names(checks)) {
+    type <- if (is.null(checks[[arg]]$class) || checks[[arg]]$class == "NULL")
+      "unknown"
+    else
+      paste0(checks[[arg]]$class, collapse = " or ")
+
+    values <- if (!is.null(checks[[arg]]$values))
+      c("allowed values {\n    ", paste0(checks[[arg]]$values, collapse = "\n    "), "\n  }, ")
+    else
+      ""
+
+    required <- if (checks[[arg]]$required) "" else "not "
+
+    cat(
+      "  ", arg, ": type ", type, ", ", values, required, "required\n",
+      sep = ""
+    )
+  }
 }
 
 check_args_internal <- function() {
@@ -83,24 +111,24 @@ check_args <- function(args, checks) {
 
     if (!is.null(check)) {
       val <- eval(args[[name]])
-      validate_arg(val, check)
+      validate_arg(name, val, check)
     }
   }
 
   invisible(TRUE)
 }
 
-validate_arg <- function(val, check) {
+validate_arg <- function(name, val, check) {
 
   # Check type: mandatory argument
   if (check$required && is.null(val)) {
-    stop(paste0(name, " is a required argument"))
+    stop(paste0(name, " is a required argument"), call. = F)
   }
 
   # Check type: class
   if (!is.null(check$class)) {
     if (length(intersect(class(val), check$class)) == 0) {
-      stop(paste0(name, " does not have any allowed class (", paste(check$class, collapse = ", "), ")"))
+      stop(paste0(name, " does not have any allowed class (", paste(check$class, collapse = ", "), ")"), call. = F)
     }
   }
 
@@ -109,25 +137,25 @@ validate_arg <- function(val, check) {
     # Value check with set
     if (is.atomic(check$values)) {
       if (!(val %in% check$values)) {
-        stop(paste0(name, " does not equal any allowed value (", paste(check$values, collapse = ", "), ")"))
+        stop(paste0(name, " does not equal any allowed value (", paste(check$values, collapse = ", "), ")"), call. = F)
       }
     } else {
       # Value check in interval
       if (!is.null(check$values$min)) {
         if (val < check$values$min) {
-          stop(paste0(name, " is lower than the minimum allowed value (", check$values$min, ")"))
+          stop(paste0(name, " is lower than the minimum allowed value (", check$values$min, ")"), call. = F)
         }
       }
       if (!is.null(check$values$max)) {
         if (val > check$values$max) {
-          stop(paste0(name, " is higher than the maximum allowed value (", check$values$max, ")"))
+          stop(paste0(name, " is higher than the maximum allowed value (", check$values$max, ")"), call. = F)
         }
       }
     }
   }
 }
 
-test_function <- function(network = arg_network(), loss = arg_loss(), weight = 2e-4) {
+test_function <- function(network = arg_network(), loss = arg_loss(), activation = arg_activation(), weight = 2e-4) {
   check_args_internal()
 }
 
