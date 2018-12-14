23040239d3502171e1663a4f123bb10c1936c3b8
David Charte <fdavidcl@protonmail.com>
2018-12-14 13:37:46 +0100

    Implement reductive AE in ruta
    Needs a better name still

--- R/autoencoder_complexity.R
+++ R/autoencoder_complexity.R
@@ -1,86 +1,95 @@
-autoencoder_complexity <- function() {
-
+autoencoder_reductive <- function(network, loss = arg_loss(), method = "fisher", weight = 0.01) {
+  autoencoder(network, loss) %>% make_reductive(method, weight)
 }
 
-to_keras.autoencoder_complexity <- function(object) {
-
+make_reductive <- function(learner, method = "fisher", weight = 0.01) {
+  learner$loss <- loss_reductive(learner$loss, method, weight)
+  class(learner) <- c(ruta_autoencoder_reductive, class(learner))
+  learner
 }
 
-example_autoencoder_complexity <- function() {
-  library(keras)
-  library(purrr)
-  library(colorspace)
-  library(scatterplot3d)
+loss_reductive <- function(reconstruction_loss, method = "fisher", weight = 0.01) {
+  structure(list(
+    reconstruction_loss = reconstruction_loss,
+    method = method,
+    weight = weight
+  ),
+  class = c(ruta_loss_reductive, ruta_loss))
+}
 
-  input_size <- 30
-  latent_dim <- 3
-
-  enc_input <- layer_input(list(input_size))
-  encoding <- layer_dense(enc_input, units = latent_dim)
-  # decodification layer object
-  decode_layer <- layer_dense(units = input_size, activation = "sigmoid")
-  # decodification layer of the autoencoder (attached to the encoder)
-  decodification <- decode_layer(encoding)
-
+to_keras.ruta_autoencoder_reductive <- function(learner, weights_file = NULL) {
+  models <- to_keras.ruta_autoencoder(learner, weights_file = weights_file)
   # class input accepts ones and zeros
   class_pos <- layer_input(list(1))
+  models$autoencoder <- keras_model(list(models$autoencoder$input, class_pos), models$autoencoder$output)
+
+  models
+}
+
+train.ruta_autoencoder_reductive <- function(
+  learner,
+  data,
+  classes,
+  epochs = 20,
+  optimizer = keras::optimizer_rmsprop(),
+  ...) {
+  learner$input_shape <- dim(data)[-1]
+  learner$models <- to_keras(learner)
+
+  loss_node <- learner$loss %>% to_keras(learner)
+  learner$models$autoencoder$add_loss(loss_node)
+
+  learner$models$autoencoder$compile(optimizer = optimizer)
+
+  input_data <- if (is.null(learner$filter)) {
+    data
+  } else {
+    apply_filter(learner$filter, data)
+  }
+
+  keras::fit(
+    learner$models$autoencoder,
+    x = list(input_data, classes),
+    epochs = epochs,
+    ...
+  )
+
+  invisible(learner)
+}
+
+to_keras.ruta_loss_reductive <- function(loss, learner) {
+  if (loss$method != "fisher") {
+    stop("Allowed complexity reduction methods: fisher")
+  }
+
+  enc_input <- learner$models$autoencoder$inputs[[1]]
+  class_pos <- learner$models$autoencoder$inputs[[2]]
+  encoding <- keras::get_layer(learner$models$autoencoder, "encoding") %>% keras::get_output_at(1)
+  decodification <- learner$models$autoencoder$output
+
   # calculate negative instances
   class_neg <- 1 - class_pos
 
   # count positive and negative instances
-  amount_pos <- k_sum(class_pos)
-  amount_neg <- k_sum(class_neg)
+  amount_pos <- keras::k_sum(class_pos) + keras::k_epsilon() # add epsilon to prevent NaN
+  amount_neg <- keras::k_sum(class_neg) + keras::k_epsilon()
 
   # keeps the value of the encoding or zero according to each instance's class
   encoding_if_pos <- class_pos * encoding
   encoding_if_neg <- class_neg * encoding
 
   # sum each feature over all instances in the batch
-  mean_pos <- k_sum(encoding_if_pos, axis = 1) / amount_pos
-  mean_neg <- k_sum(encoding_if_neg, axis = 1) / amount_neg
+  mean_pos <- keras::k_sum(encoding_if_pos, axis = 1) / amount_pos
+  mean_neg <- keras::k_sum(encoding_if_neg, axis = 1) / amount_neg
 
   # similarly calculate variance as E[X^2] - E[X]^2
-  variance_pos <- k_sum(k_square(encoding_if_pos), axis = 1) / amount_pos - k_square(mean_pos)
-  variance_neg <- k_sum(k_square(encoding_if_neg), axis = 1) / amount_neg - k_square(mean_neg)
+  variance_pos <- keras::k_sum(keras::k_square(encoding_if_pos), axis = 1) / amount_pos - keras::k_square(mean_pos)
+  variance_neg <- keras::k_sum(keras::k_square(encoding_if_neg), axis = 1) / amount_neg - keras::k_square(mean_neg)
 
-  fisher_ratios <- k_square(mean_pos - mean_neg) * (1 / (variance_pos + variance_neg))
-  fisher_gain <- k_max(fisher_ratios)
+  fisher_ratios <- keras::k_square(mean_pos - mean_neg) * (1 / (variance_pos + variance_neg + keras::k_epsilon()))
+  fisher_gain <- keras::k_max(fisher_ratios)
 
-  rec_loss <- loss_binary_crossentropy(enc_input, decodification)
-  regularized_loss <- (rec_loss %>% k_mean()) - fisher_gain
-
-  encoder <- keras_model(enc_input, encoding)
-  #decoder_input <- layer_input(latent_dim)
-  #decoder <- keras_model(decoder_input, decode_layer(decoder_input))
-  ae <- keras_model(list(enc_input, class_pos), decodification)
-
-  ae$add_loss(regularized_loss)
-  ae$compile(optimizer_rmsprop(lr = 0.01))
-
-  # ae %>% compile("rmsprop", "mean_squared_error")
-
-  wdbc <- read.csv("wdbc.data")
-  x_train <- wdbc[, 3:32]
-  mx <- apply(x_train, 2, max)
-  mn <- apply(x_train, 2, min)
-  range <- mx - mn
-  x_train <- t(apply(x_train, 1, function(x) (x - mn) / range))
-  y_train <- ifelse(wdbc[, 2] == "M", 1, 0)
-
-  fit(
-    ae,
-    list(x_train, y_train),
-    epochs = 500,
-    batch_size = 32
-  )
-  scatterplot3d((encoder %>% predict(x_train))[, c(2,1,3)], color = rainbow_hcl(2)[y_train + 1], pch = 20)
-
-  ae %>% compile("rmsprop", "mean_squared_error")
-  evaluate(ae, list(x_train, y_train), y = x_train)
-
-  #### normal autoencoder
-
-  library(ruta)
-  codes <- x_train %>% autoencode(3, epochs = 500)
-  scatterplot3d(codes, color = rainbow_hcl(2)[y_train + 1], pch = 20)
+  rec_loss <- (loss$reconstruction_loss %>% to_keras(learner))(enc_input, decodification)
+  regularized_loss <- (rec_loss %>% keras::k_mean()) - loss$weight * fisher_gain
+  regularized_loss
 }
